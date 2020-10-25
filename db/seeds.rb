047f4ecf5ac2bdbcc71d483f56c2b26869d314a3
philipd <philip.dickau@gmail.com>
2020-10-25 11:40:41 -0600

    Seed example shows

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Show.destroy_all
 Broadcaster.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('broadcasters')
+ActiveRecord::Base.connection.reset_pk_sequence!('shows')
 
 Broadcaster.create(handle: 'purple-finch', name: 'Purple Finch Podcasts', description: 'Just a gang of pals with stories to tell')
 Broadcaster.create(handle: 'wxfr', name: 'WXFR', description: 'WXFR Community Radio, broadcasting out of the offices of Yoyodyne industries')
 Broadcaster.create(handle: 'radio70k', name: 'Radio 70000', description: 'Independent radio')
+
+Show.create(name: 'Modern Expansion', broadcaster_id: 1)
+Show.create(name: 'Contemporary Few', broadcaster_id: 1)
+Show.create(name: 'Syntactic Yacht', broadcaster_id: 2)
+Show.create(name: 'Urban Homework', broadcaster_id: 2)
+Show.create(name: 'Complete Herb', broadcaster_id: 2)
+Show.create(name: 'Rude Executive', broadcaster_id: 3)
+Show.create(name: 'Negative Bank', broadcaster_id: 3)
+Show.create(name: 'Coming Occasion', broadcaster_id: 3)
