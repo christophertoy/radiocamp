403b73f8794237d6bdc3f218804463302b904fd1
philipd <philip.dickau@gmail.com>
2020-10-25 09:55:30 -0600

    Create 3 example broadcasters in the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Broadcaster.destroy_all
+
+Broadcaster.create(handle: 'purple-finch', name: 'Purple Finch Podcasts', description: 'Just a gang of pals with stories to tell')
+Broadcaster.create(handle: 'wxfr', name: 'WXFR', description: 'WXFR Community Radio, broadcasting out of the offices of Yoyodyne industries')
+Broadcaster.create(handle: 'radio70k', name: 'Radio 70000', description: 'Independent radio')
