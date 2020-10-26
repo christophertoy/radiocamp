5287a6ba89b41635a62534479cdcc6653232796f
Christopher Toy <christopherstoy@gmail.com>
2020-10-26 11:29:46 -0600

    create list component and story

---
+++ app/javascript/packs/components/List.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ShowListItem from "./ShowListItem";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  }
+}));
+
+export default function showList(props) {
+  const classes = useStyles();
+  // map over array of props and pass them down to ShowListItem
+  return (
+    <div className={classes.root}>
+    <Typography variant="h4">
+      Shows
+    </Typography>
+      <ShowListItem></ShowListItem>
+      <ShowListItem></ShowListItem>
+      <ShowListItem></ShowListItem>
+    </div>
+  );
+}
