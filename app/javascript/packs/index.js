aa3888ea4273c3e318abeb33533f6eece37825cc
philipd <philip.dickau@gmail.com>
2020-10-25 21:27:46 -0600

    Display NavBar on home page

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -5,11 +5,13 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import Aloha from './components/Aloha';
+import NavBar from './components/NavBar';
 require('./css/application.css')
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Aloha name="React" />,
+    // <Aloha name="React" />,
+    <NavBar />,
     document.getElementById("root")
     // document.body.appendChild(document.createElement('div')),
   )
