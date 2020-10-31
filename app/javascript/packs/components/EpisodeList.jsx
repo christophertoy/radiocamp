29b44019611b23959756108bd3902cf573600e4c
philipd <philip.hd@protonmail.com>
2020-10-30 19:11:19 -0600

    Move EpisodeForm to EpisodeList

--- app/javascript/packs/components/EpisodeList.jsx
+++ app/javascript/packs/components/EpisodeList.jsx
@@ -5,6 +5,7 @@ import EpisodeList from "./EpisodeListItem";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import EpisodeListItem from "./EpisodeListItem";
+import EpisodeForm from "./EpisodeForm";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,6 +21,12 @@ export default function episodeList(props) {
   return (
     <div className={classes.root}>
       <Typography variant="h4">Episodes</Typography>
+      {props.currentUser === props.broadcasterData.handle && (
+        <EpisodeForm 
+          broadcasterId={props.broadcasterData.id} 
+          showId={props.showId}
+          setEpisodes={props.setEpisodes}/>
+      )}
       {props.episodes.map((episode, index) => {
         return (
           <EpisodeListItem
