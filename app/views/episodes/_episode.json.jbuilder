18f97d7769aa1265bc1a69a732f8c2f045c7a9ed
philipd <philip.hd@protonmail.com>
2020-11-02 14:54:52 -0700

    Add 'image' column to 'episodes' table in db, and return it in jason endpoints

--- app/views/episodes/_episode.json.jbuilder
+++ app/views/episodes/_episode.json.jbuilder
@@ -1,4 +1,4 @@
-json.extract! episode, :id, :title, :description, :episode_url, :embed_code, :release_date, :show_id, :episode_number, :created_at, :updated_at
+json.extract! episode, :id, :title, :description, :episode_url, :embed_code, :release_date, :show_id, :episode_number, :created_at, :updated_at, :image
 json.url episode_url(episode, format: :json)
 json.showName Show.find(episode.show_id).name
-json.image Show.find(episode.show_id).image
+json.show_image Show.find(episode.show_id).image
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_020023) do
+ActiveRecord::Schema.define(version: 2020_11_02_214851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_10_30_020023) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "embed_code"
     t.bigint "episode_number"
+    t.string "image"
     t.index ["show_id"], name: "index_episodes_on_show_id"
   end
 
