a3bffb960a41a2187769d2a48ed3aecae1c45a85
Edward Smith <edward.m.smith80@gmail.com>
2020-11-01 23:04:00 -0700

    remove console log

--- app/javascript/packs/components/EpisodeListItem.jsx
+++ app/javascript/packs/components/EpisodeListItem.jsx
@@ -14,7 +14,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function EpisodeListItem(props) {
-  console.log('props from epilistitem', props);
   const classes = useStyles();
   let match = useRouteMatch();
 
