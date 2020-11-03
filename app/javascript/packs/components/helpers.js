2f428065b5f4dfab36fd386fffa02a718be84a19
philipd <philip.hd@protonmail.com>
2020-11-03 09:15:28 -0700

    Fix mixcloud widgets to bottom of page by setting iframe height to 120px

--- app/javascript/packs/components/helpers.js
+++ app/javascript/packs/components/helpers.js
@@ -5,7 +5,7 @@ export const getEmbedCode = (url) => {
   if(url && url.includes("www.mixcloud.com")) {
     url = url.split("/");
     url = ["'https://www.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2F", url[3], "%2F", url[4], "%2F'"].join("");    
-    return `<iframe width='100%' height='152' src=${url} frameborder='0' ></iframe>`;
+    return `<iframe width='100%' height='120' src=${url} frameborder='0' ></iframe>`;
   } else if (url && url.includes("spotify.com")) {
     url = url.split("/");
     url = ["'https://open.spotify.com/embed-podcast/episode/", url[4], "'"].join("");
