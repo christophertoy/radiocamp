77bdf2e013124c1f937bf0a0ef03982bbe623349
philipd <philip.hd@protonmail.com>
2020-11-03 17:15:19 -0700

    Validate handles on broadcaster creation

--- app/models/broadcaster.rb
+++ app/models/broadcaster.rb
@@ -1,4 +1,4 @@
 class Broadcaster < ApplicationRecord
   has_many :shows
-  validates :handle, presence: true, uniqueness: true, format: { with: /\A[a-z0-9\-]\z/ }
+  validates :handle, presence: true, uniqueness: true, format: { with: /\A[a-z0-9\-]*\z/ }
 end
